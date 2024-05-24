fa9e089a320f73961dc693d706ffc07243080a6a
josh-boat365 <nyarko.boateng20@outlook.com>
2024-05-24 06:42:48 +0000

    increased size of logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,9 +18,10 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-[30rem] h-[30rem] fill-current text-gray-500" />
                 </a>
             </div>
+            <h3 class="font-bold text-green-600">TRIUMPHANT CHRISTIAN CENTER - MEMBERSHIP DATABSE</h3>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
