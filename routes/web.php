719ea6261bc078cf4ebc85b31631ae6cabe5d693
josh-boat365 <nyarko.boateng20@outlook.com>
2024-05-24 06:41:32 +0000

    established routes for dashboard crud applications

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Member;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\MemberController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\MemberController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 
@@ -15,6 +16,17 @@ Route::middleware('auth')->group(function () {
     Route::get('dashboard', [MemberController::class, 'index'])->middleware(['verified'])->name('dashboard');
 
     Route::post('save-member', [MemberController::class, 'store'])->name('save-member');
+
+    Route::get('/search-members', [MemberController::class, 'search'])->name('search-members');
+    Route::get('/delete-member/{id}', [MemberController::class, 'destroy'])->name('delete-member');
+    Route::get('/update-member/{id}', [MemberController::class, 'update'])->name('update-member');
+    Route::get('/export-pdf', [MemberController::class, 'exportPDF'])->name('export-pdf');
+
+    Route::get('/download-members-pdf', [MemberController::class, 'downloadPDF'])->name('download.members.pdf');
+
+    Route::get('test', function(){
+        $members = Member::all();
+         return view('pdf.members', compact('members'));});
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
