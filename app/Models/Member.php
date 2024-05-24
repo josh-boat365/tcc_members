a8aa89ab751c97d34e6805614dd786db6eb436a8
josh-boat365 <nyarko.boateng20@outlook.com>
2024-05-24 06:50:08 +0000

    added search function to search for members

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -12,10 +12,29 @@ class Member extends Model
         'code',
         'first_name',
         'last_name',
+        'gender',
         'contact_1',
         'contact_2',
         'location',
         'department',
+        'group',
         'image',
+        'year_joined',
     ];
+
+    public function scopeSearchMembers($query, $search)
+    {
+        return $query->where(function ($query) use ($search) {
+            $query->where('code', 'like', '%' . $search . '%')
+                ->orWhere('first_name', 'like', '%' . $search . '%')
+                ->orWhere('last_name', 'like', '%' . $search . '%')
+                ->orWhere('gender', 'like', '%' . $search . '%')
+                ->orWhere('contact_1', 'like', '%' . $search . '%')
+                ->orWhere('contact_2', 'like', '%' . $search . '%')
+                ->orWhere('location', 'like', '%' . $search . '%')
+                ->orWhere('department', 'like', '%' . $search . '%')
+                ->orWhere('group', 'like', '%' . $search . '%')
+                ->orWhere('year_joined', 'like', '%' . $search . '%');
+        });
+    }
 }
