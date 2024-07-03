61ef582003d53028daba0a2a6bfee98d37843e3f
josh-boat365 <nyarko.boateng20@outlook.com>
2024-07-03 14:29:47 +0000

    modified updated members view

--- resources/views/partials/member_table.blade.php
+++ resources/views/partials/member_table.blade.php
@@ -1,11 +1,7 @@
-@forelse ($members as $member)
+@forelse ($members as $index => $member)
     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
         <td class="w-4 p-4">
-            <div class="flex items-center">
-                <input id="checkbox-table-search-1" type="checkbox"
-                    class="w-8 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-            </div>
+            {{ $member->id }}
         </td>
         <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
             <img class="w-10 h-10 rounded-full"
